6b98a4cf25f2f39f73cfd85e184d17aa0ac1eeed
Sonu <sonu3660@gmail.com>
2020-08-27 00:18:01 +0530

    Creating Comment with some changes

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -24,13 +24,13 @@ export const USER_PROFILE_SUCCESS = 'USER_PROFILE_SUCCESS';
 export const USER_PROFILE_FAILURE = 'USER_PROFILE_FAILURE';
 export const FETCH_USER_PROFILE = 'FETCH_USER_PROFILE';
 
-// friends
+// Add friends
 export const FETCH_FRIENDS_SUCCESS = 'FETCH_FRIENDS_SUCCESS';
 export const ADD_FRIEND = 'ADD_FRIEND';
 export const REMOVE_FRIEND = 'REMOVE_FRIEND';
 
-// Posts
+// Add Posts
 export const ADD_POST = 'ADD_POST';
 
-// Comments
+//Add Comments
 export const ADD_COMMENT = 'ADD_COMMENT';
