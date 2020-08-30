982551ac60eb25b48d904490cb5a454f67513ff3
Sonu <sonu3660@gmail.com>
2020-08-30 13:54:27 +0530

    Chat: using socket

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,17 +1,85 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+import { connect } from 'react-redux';
 
 import '../chat.css';
 
 class Chat extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       messages: [], //{content: 'some message', self: true/false}
       typedMessage: '',
     };
+
+    // Create connection with socket.io
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+    console.log('PROPS', props);
+
+    // Setup connection with server
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+
+    // use this method to define an actions
+    this.socket.on('connect', function () {
+      console.log('Connection Established!');
+
+      // Send the action by "emit()" to server with some data
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      //   Listen for the user join us
+      socketConnection.on('user_joined', function (data) {
+        console.log('New User Joined', data);
+      });
+    });
+
+    // Setup listener for receive messages
+    this.socket.on('receive_message', function (data) {
+      // Add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage, messages } = this.state;
+
+    console.log('Messages', messages);
+
+    if (typedMessage && this.userEmail) {
+      //   Send an Action
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
+
   render() {
     const { typedMessage, messages } = this.state;
+    // console.log('Messages', messages);
     return (
       <div className="chat-container">
         <div className="chat-header">
@@ -25,6 +93,7 @@ class Chat extends Component {
         <div className="chat-messages">
           {messages.map((message) => (
             <div
+              key={message}
               className={
                 message.self
                   ? 'chat-bubble self-chat'
@@ -48,4 +117,10 @@ class Chat extends Component {
   }
 }
 
-export default Chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+
+export default connect(mapStateToProps)(Chat);
--- src/components/index.js
+++ src/components/index.js
@@ -1,32 +1,3 @@
-// This file is responsible for import and export the components
-// import PostsList from './PostsList';
-// import App from './App';
-// import Navbar from './Navbar';
-// import Home from './Home';
-// import Page404 from './Page404';
-// import Login from './Login';
-// import Signup from './Signup';
-// import Settings from './Settings';
-// import UserProfile from './UserProfile';
-// import FriendsList from './FriendsList';
-// import FriendsListItem from './FriendsListItem';
-// import CreatePost from './CreatePost';
-
-// export {
-//   PostsList,
-//   App,
-//   Navbar,
-//   Home,
-//   Page404,
-//   Login,
-//   Signup,
-//   Settings,
-//   UserProfile,
-//   FriendsList,
-//   FriendsListItem,
-//   CreatePost,
-// };
-
 import PostsList from './PostsList';
 import App from './App';
 import Navbar from './Navbar';
